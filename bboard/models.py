aaaba87b4f1b69b4b7bff650124dc319666d2a81
Андрей Шибченко <@itstep.loc>
2023-04-19 17:50:38 +0600

    losson_02 files

---
+++ bboard/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Bb(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name="Товар"
+    )
+
+    content = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Оисание"
+    )
+
+    price = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name="Цена"
+
+    )
+    published = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name="Опубликовано"
+    )
+
+    def __str__(self):
+        return 'Объявление: ' + self.title
+
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+        ordering = ['-published']
