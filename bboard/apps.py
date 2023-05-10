394305d12ef36975b7e2435c0ec92a7a4d4804ec
Андрей Шибченко <@itstep.loc>
2023-05-10 15:08:35 +0600

    lesson_04

---
+++ bboard/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class BboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bboard'
+    verbose_name = 'Доска объявлений'
+
